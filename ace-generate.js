13c916d8458b5565eb01fe766dd112d21875ac52
Tobbe <tobias.brolin@atex.com>
2018-09-21 13:13:28 +0200

    Added a content generation script

---
+++ ace-generate.js
@@ -0,0 +1,99 @@
+#!/usr/bin/env node
+
+const cmd = require('commander'),
+  fs = require('fs'),
+  axios = require('axios'),
+  doT = require('doT'),
+  uuid =  require('uuid/v4');
+
+cmd
+  .usage ('[options] <template> <numberOfContent>')
+  .description ('Creates numberOfContent number of content from template.')
+  .option ('-t, --token <token>', 'use token, if not provided trying to use $TOKEN from environment')
+  .option ('-o, --output-format <format>', 'format of output (raw | pretty | console)','pretty')
+  .option ('-P --port <port>', 'host port', '8080')
+  .option ('-H --host <host>', 'host', 'localhost')
+  .option ('-S --path <path>', 'path to service', '/content-service')
+  .parse (process.argv);
+
+  let token = cmd.token || process.env.TOKEN,
+      data = cmd.args[0],
+      numberOfContent = parseInt(cmd.args[1]),
+      outputFormat = cmd.outputFormat,
+      params = {},
+      url = 'http://' + cmd.host + ':' + cmd.port + cmd.path + '/content';
+
+  if (!token) {
+    console.error ('Cannot make request - no token present');
+    cmd.help ();
+  }
+
+  try {
+    if (data.startsWith ('@')) {
+        template = fs.readFileSync (data.slice(1), 'utf8');
+    } else {
+        template = JSON.stringify (JSON.parse (data));
+    }
+
+    const createContent = function (content) {
+      return axios.post (url, content,  { headers: { 'content-type': 'application/json', 'x-auth-token': token }, params: params })
+      .catch(function (error) {
+        if (error.response && error.response.data) {
+          console.error(error.response.data.message);
+        } else {
+          console.log(error);
+        }
+        process.exit(1);
+      });
+    }
+
+    const generateData = doT.template (template);
+    let counter = 0;
+    const intervalId = setInterval (function () {
+      counter++;
+      if (counter === numberOfContent) {
+        clearInterval(intervalId);
+        createContent (generateData ({ uniq: uuid () }))
+          .then (function () {
+            process.stdout.clearLine();
+            process.stdout.cursorTo(0);
+            process.stdout.write(counter + '/' + numberOfContent + ' - Done.\n') ;
+          });
+      } else {
+        createContent (generateData ({ uniq: uuid () }));
+        process.stdout.clearLine();
+        process.stdout.cursorTo(0);
+        process.stdout.write(counter + '/' + numberOfContent);
+      }
+    }, 50);
+
+  } catch (error) {
+      console.error (error.message);
+      process.exit (1);
+  }
+
+/*
+  axios.post (url, data,
+    { headers: { 'content-type': 'application/json',
+                 'x-auth-token': token },
+      params: params })
+    .then (function (response) {
+      if (outputFormat === 'raw') {
+        console.log (JSON.stringify(response.data));
+      } else if (outputFormat === 'pretty') {
+        console.log (JSON.stringify(response.data, null, 2));
+      } else if (outputFormat === 'console') {
+        console.dir (response.data, { depth: null, colors: true });
+      } else {
+        console.error ('Unknown output format "' + outputFormat + '". Defaulting to "pretty"');
+        console.log (JSON.stringify(response.data, null, 2));
+      }
+    })
+    .catch (function (error) {
+      // console.error ('Could not create content -', error);
+      let msg = error.response.status || 'NO_STATUS';
+      if (error && error.response && error.response.data) {
+        msg += ' - ' + error.response.data.message;
+      }
+      console.error (msg);
+    }); */
--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const cmd = require('commander'),
 cmd
   .version (version)
   .description ('Collection of useful scripts when working with an ace system.')
-  .command ('token', 'Get auth token')
   .command ('create', 'Create a content')
+  .command ('generate <template> <n>', 'Generate n content from template')
   .command ('read <alias> [view]', 'Read content by alias [from view]')
   .command ('reindex', 'Issue a reindexing of content')
+  .command ('token', 'Get auth token')
   .parse (process.argv);
