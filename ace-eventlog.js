08633c30031b8ee78af72093ef3f4fa6f086fd8d
Tobbe <tobias.brolin@atex.com>
2018-10-26 10:52:15 +0200

    Possible to specify a kafka groupId

--- ace-eventlog.js
+++ ace-eventlog.js
@@ -5,6 +5,7 @@ cmd
   .usage ('[options]')
   .option ('--verbose', 'Extend output with metadata')
   .option ('--from-beginning', 'Play events from beginning of time', false)
+  .option ('--logger-id <name>', 'Use a consumer with <name> to be able to have independent loggers', 'ace-cli-event-logger')
   .option ('--from-offset <offset>', 'Play events from <offset>')
   .option ('-P --port <port>', 'host port', '9092')
   .option ('-H --host <host>', 'host', 'localhost')
@@ -19,7 +20,7 @@ if (cmd.fromOffset || parseInt (cmd.fromOffset) === 0) {
 
 const connectionString = cmd.host + ':' + cmd.port;  
 const client = new kafka.KafkaClient(connectionString);
-const consumer = new kafka.Consumer (client, [{ topic: 'aceEvents', offset }], { fromOffset: cmd.fromBeginning });
+const consumer = new kafka.Consumer (client, [{ topic: 'aceEvents', offset }], { groupId: cmd.loggerId, fromOffset: cmd.fromBeginning });
 
 consumer.on ('message', (message) => {
   if (cmd.verbose) {
