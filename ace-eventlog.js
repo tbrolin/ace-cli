672fa6b0308e29ec64a94f10395a29b7b0ad206b
Tobbe <tobias.brolin@atex.com>
2018-12-05 14:02:22 +0100

    Follow ace subscription events rather than system events

--- ace-eventlog.js
+++ ace-eventlog.js
@@ -7,27 +7,35 @@ cmd
   .option ('--from-beginning', 'Play events from beginning of time', false)
   .option ('--logger-id <name>', 'Use a consumer with <name> to be able to have independent loggers', 'ace-cli-event-logger')
   .option ('--from-offset <offset>', 'Play events from <offset>')
+  .option ('--system', 'Follow system events rather than subscription events.')
   .option ('-P --port <port>', 'host port', '9092')
   .option ('-H --host <host>', 'host', 'localhost')
   .parse (process.argv);
 
 let offset = 0;
+let topic = 'aceContentEvents'
 
 if (cmd.fromOffset || parseInt (cmd.fromOffset) === 0) {
   cmd.fromBeginning = true;
   offset = parseInt (cmd.fromOffset);
 }
 
+if (cmd.system) {
+  topic = 'aceEvents';
+}
+
 const connectionString = cmd.host + ':' + cmd.port;  
 const client = new kafka.KafkaClient(connectionString);
-const consumer = new kafka.Consumer (client, [{ topic: 'aceEvents', offset }], { groupId: cmd.loggerId, fromOffset: cmd.fromBeginning });
+const consumer = new kafka.Consumer (client, [{ topic, offset }], { groupId: cmd.loggerId, fromOffset: cmd.fromBeginning });
 
 consumer.on ('message', (message) => {
   if (cmd.verbose) {
     console.log ();
     console.log (message);
   } else {
-    console.log (message.offset + ': ' + JSON.parse(message.value).payload);
+    let payload = JSON.parse(message.value);
+    payload = payload.payload ? JSON.stringify(payload.payload) : JSON.stringify(JSON.parse(message.value), null, 2);
+    console.log (message.offset + ': ' + payload);
   }
 });
 
